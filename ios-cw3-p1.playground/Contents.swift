af534059afdff93939b2abb8231ce132edeac234
hussainashkanani-150 <hashkanani02@gmail.com>
2020-06-20 20:47:04 +0300

    تم حل الكلاس وورك
    done

---
+++ ios-cw3-p1.playground/Contents.swift
@@ -0,0 +1,11 @@
+
+var number1 = 2
+var number2 = 5
+
+func add(a: Int, b: Int) -> Int
+{
+return a + b
+}
+
+var functionCall = add(a: number1, b: number2)
+print ("\(number1) + \(number2) = \(functionCall)")
